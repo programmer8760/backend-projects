4ace3c913334c26c702820e97a86c4b38fb18c4b
programmer8760 <programmer8760@gmail.com>
2025-04-23 01:39:53 +0300

    feat: html+css structure completion

--- unit-converter/app.go
+++ unit-converter/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"math"
 )
 
 // App struct
@@ -24,4 +25,10 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) ConvertLength(length float64, from int, to int) string {
+	scales := [8]float64{0.001, 0.01, 1, 1000, 0.0254, 0.30478, 0.91407, 0.0006215}
+	unitsNames := [8]string{"mm", "cm", "m", "km", "in", "ft", "yd", "mi"}
+	return fmt.Sprintf("%v%s = %v%s", length, unitsNames[from], math.Round(length*scales[from]/scales[to]*10000)/10000, unitsNames[to])
 }
--- unit-converter/frontend/src/App.jsx
+++ unit-converter/frontend/src/App.jsx
@@ -1,67 +1,140 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
+import { ConvertLength } from "../wailsjs/go/main/App";
 
 function App() {
     const [currentPage, setPage] = useState("length");
+    const [result, setResult] = useState(null);
 
-    const renderPage = () => {
+    let convertedValue;
+
+    const convertLength = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const length = parseFloat(formData.get("length"));
+        const from = parseInt(formData.get("from"));
+        const to = parseInt(formData.get("to"));
+        ConvertLength(length, from, to).then((response) => {
+            setResult(response);
+        });
+    }
+
+    const reset = () => {
+        setResult(null);
+    }
+
+    const renderForm = () => {
+        if(result != null) {
+            return;
+        }
         switch (currentPage) {
             case "length":
                 return (
-                    <form>
+                    <form onSubmit={convertLength} id="length-form">
                         <label className="label">Enter the length to convert</label>
-                        <input className="input-field"></input>
+                        <input name="length" className="input-field"></input>
                         <label className="label">Unit to convert from</label>
-                        <select className="selector">
-                            <option value="millimeter">Millimeter</option>
-                            <option value="millimeter">Centimeter</option>
-                            <option value="millimeter">Meter</option>
-                            <option value="millimeter">Kilometer</option>
-                            <option value="millimeter">Inch</option>
-                            <option value="millimeter">Foot</option>
-                            <option value="millimeter">Yard</option>
-                            <option value="millimeter">Mile</option>
+                        <select name="from" className="selector" form="length-form">
+                            <option value="0">Millimeter</option>
+                            <option value="1">Centimeter</option>
+                            <option selected="selected" value="2">Meter</option>
+                            <option value="3">Kilometer</option>
+                            <option value="4">Inch</option>
+                            <option value="5">Foot</option>
+                            <option value="6">Yard</option>
+                            <option value="7">Mile</option>
                         </select>
                         <label className="label">Unit to convert to</label>
-                        <select className="selector">
-                            <option value="millimeter">Millimeter</option>
-                            <option value="millimeter">Centimeter</option>
-                            <option value="millimeter">Meter</option>
-                            <option value="millimeter">Kilometer</option>
-                            <option value="millimeter">Inch</option>
-                            <option value="millimeter">Foot</option>
-                            <option value="millimeter">Yard</option>
-                            <option value="millimeter">Mile</option>
+                        <select name="to" className="selector" form="length-form">
+                            <option value="0">Millimeter</option>
+                            <option value="1">Centimeter</option>
+                            <option selected="selected" value="2">Meter</option>
+                            <option value="3">Kilometer</option>
+                            <option value="4">Inch</option>
+                            <option value="5">Foot</option>
+                            <option selected="selected" value="6">Yard</option>
+                            <option value="7">Mile</option>
                         </select>
+                        <button type="submit" className="button">Convert</button>
                     </form>
                 );
             case "weight":
                 return (
-                    <form>
+                    <form id="weight-form">
                         <label className="label">Enter the weight to convert</label>
-                        <input className="input-field"></input>
+                        <input name="weight" className="input-field"></input>
+                        <label className="label">Unit to convert from</label>
+                        <select name="from" className="selector" form="weight-form">
+                            <option value="milligram">Milligram</option>
+                            <option value="gram">Gram</option>
+                            <option selected="selected" value="kilogram">Kilogram</option>
+                            <option value="ounce">Ounce</option>
+                            <option value="pound">Pound</option>
+                        </select>
+                        <label className="label">Unit to convert to</label>
+                        <select name="to" className="selector" form="weight-form">
+                            <option value="milligram">Milligram</option>
+                            <option value="gram">Gram</option>
+                            <option value="kilogram">Kilogram</option>
+                            <option value="ounce">Ounce</option>
+                            <option selected="selected" value="pound">Pound</option>
+                        </select>
+                        <button type="submit" className="button">Convert</button>
                     </form>
+                    
                 );
             case "temperature":
                 return (
-                    <form>
+                    <form id="temperature-form">
                         <label className="label">Enter the temperature to convert</label>
-                        <input className="input-field"></input>
+                        <input name="temperature" className="input-field"></input>
+                        <label className="label">Unit to convert from</label>
+                        <select name="from" className="selector" form="temperature-form">
+                            <option selected="selected" value="celsius">Celsius</option>
+                            <option value="fahrenheit">Fahrenheit</option>
+                            <option value="kelvin">Kelvin</option>
+                        </select>
+                        <label className="label">Unit to convert to</label>
+                        <select name="to" className="selector" form="temperature-form">
+                            <option value="celsius">Celsius</option>
+                            <option selected="selected" value="fahrenheit">Fahrenheit</option>
+                            <option value="kelvin">Kelvin</option>
+                        </select>
+                        <button type="submit" className="button">Convert</button>
                     </form>
                 );
+            default: 
+                return (
+                    <p>?</p>
+                )
         }
     };
 
+    const renderResult = () => {
+        if (result != null) {
+            return (
+                <div>
+                    <div className="label">Result of your calculation</div>
+                    <div className="big-text">{result}</div>
+                    <button onClick={reset} className="button">Reset</button>
+                </div>
+            );
+
+        }
+    }
+
     return (
         <div id="App">
-            <h1 id="title">Unit Converter</h1>
+            <h1 className="big-text">Unit Converter</h1>
             <div id="topbar" className="topbar">
                 <div id="topbar-length" className={currentPage == "length" ? "topbar-button  selected" : "topbar-button"} onClick={() => setPage("length")}>Length</div>
                 <div id="topbar-weight" className={currentPage == "weight" ? "topbar-button  selected" : "topbar-button"} onClick={() => setPage("weight")}>Weight</div>
                 <div id="topbar-temperature" className={currentPage == "temperature" ? "topbar-button  selected" : "topbar-button"} onClick={() => setPage("temperature")}>Temperature</div>
             </div>
             <div id="workspace" className="workspace">
-                {renderPage()}
+                {renderForm()}
+                {renderResult()}
             </div>
         </div>
     )
